1a5a6b8f98ef677479ea742f18030013333d6cc3
pingerr <lizhaoping666@gmail.com>
2023-07-10 23:01:45 +0800

    fix:ip_deny 解决目录依赖问题

--- ip_deny/main.go
+++ ip_deny/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
 	"github.com/tidwall/gjson"
+	"ip_deny/ipfilter"
 )
 
 func main() {
@@ -17,7 +18,7 @@ func main() {
 }
 
 type IpConfig struct {
-	f IPFilter
+	f ipfilter.IPFilter
 }
 
 func parseConfig(json gjson.Result, config *IpConfig, log wrapper.Log) error {
@@ -41,7 +42,9 @@ func onHttpRequestHeaders(ctx wrapper.HttpContext, config IpConfig, log wrapper.
 	xRealIp, _ := proxywasm.GetHttpRequestHeader("x-real-ip")
 	log.Infof("[xRealIp: %]", xRealIp)
 	if config.f.FilterIPString(xRealIp) {
-		proxywasm.SendHttpResponse(403, nil, []byte("denied by ip"), -1)
+		if err := proxywasm.SendHttpResponse(403, nil, []byte("denied by ip"), -1); err != nil {
+			panic(err)
+		}
 	}
 	return types.ActionContinue
 }
