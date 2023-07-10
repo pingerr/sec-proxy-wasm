b23f69465b9551e78cccc30aee228f32ed599dc0
pingerr <lizhaoping666@gmail.com>
2023-07-10 23:38:18 +0800

    feat:ip_deny 只打印超过15字符的ip

--- ip_deny/main.go
+++ ip_deny/main.go
@@ -40,7 +40,9 @@ func parseConfig(json gjson.Result, config *IpConfig, log wrapper.Log) error {
 
 func onHttpRequestHeaders(ctx wrapper.HttpContext, config IpConfig, log wrapper.Log) types.Action {
 	xRealIp, _ := proxywasm.GetHttpRequestHeader("x-real-ip")
-	log.Infof("[xRealIp: %]", xRealIp)
+	if length := len(xRealIp); length > 15 {
+		log.Infof("[xRealIp: %s]", xRealIp)
+	}
 	if config.f.FilterIPString(xRealIp) {
 		if err := proxywasm.SendHttpResponse(403, nil, []byte("denied by ip"), -1); err != nil {
 			panic(err)
