4e63863ad2baab5be111218a9bad1439dc153f47
work <429304117@qq.com>
2023-07-20 13:47:33 +0800

    feat:ip(all)+waf(sqli)

--- ip_deny/no/no.go
+++ ip_deny/no/no.go
@@ -2,6 +2,7 @@ package no
 
 import (
 	"github.com/alibaba/higress/plugins/wasm-go/pkg/wrapper"
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
 	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
 	"github.com/tidwall/gjson"
 	"github.com/yl2chen/cidranger"
@@ -20,9 +21,13 @@ func FilterStart() {
 }
 
 func parseConfig(json gjson.Result, config *IpConfig, log wrapper.Log) error {
+	config.f = cidranger.NewPCTrieRanger()
 	return nil
 }
 
 func onHttpRequestHeaders(ctx wrapper.HttpContext, config IpConfig, log wrapper.Log) types.Action {
+	if err := proxywasm.SendHttpResponse(403, nil, []byte("denied by ip"), -1); err != nil {
+		panic(err)
+	}
 	return types.ActionContinue
 }
