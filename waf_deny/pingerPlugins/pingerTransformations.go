6f214b3f00fecfb04e9e33eaf50f573bbb90efe3
pingerr <lizhaoping666@gmail.com>
2023-07-22 18:18:43 +0800

    feat:ip(no)+waf(xss,lfi,rfi,ua,protocol,lfi-base64flat)

--- waf_deny/pingerPlugins/pingerTransformations.go
+++ waf_deny/pingerPlugins/pingerTransformations.go
@@ -1,8 +1,10 @@
 package pingerPlugins
 
 import (
+	"bytes"
 	"encoding/base64"
 	"github.com/corazawaf/coraza/v3/experimental/plugins"
+	"strings"
 	"waf_deny/pingerPlugins/utils"
 )
 
@@ -10,7 +12,12 @@ import (
 func RegisterPingerTransformations() error {
 
 	base64flatDecode := func(input string) (string, bool, error) {
-		dec, err := base64.StdEncoding.DecodeString(input)
+		var inputBuf bytes.Buffer
+		inputBuf.WriteString(input)
+		if l := len(input); l%4 != 0 {
+			inputBuf.WriteString(strings.Repeat("=", 4-l%4))
+		}
+		dec, err := base64.StdEncoding.DecodeString(inputBuf.String())
 		if err != nil {
 			// Forgiving implementation, which ignores invalid characters
 			return input, false, nil
