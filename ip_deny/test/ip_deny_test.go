24bd750fd5cd01d272ee31b2e48df2305670b0fe
pingerr <lizhaoping666@gmail.com>
2023-07-28 01:45:55 +0800

    feat:cc(bucket)+ip(radix) perf

--- ip_deny/test/ip_deny_test.go
+++ ip_deny/test/ip_deny_test.go
@@ -3,7 +3,7 @@ package test
 import (
 	"bufio"
 	"fmt"
-	"ip_deny/cidranger"
+	"ip_deny/radixTree"
 	"os"
 	"testing"
 )
@@ -27,7 +27,8 @@ func BenchmarkIp(b *testing.B) {
 	}
 
 	for n := 0; n < b.N; n++ {
-		cidranger.CidrangerTest(ipArr) // BenchmarkIp            5           3651340 ns/op          809230 B/op      43798 allocs/op
+		//cidranger.CidrangerTest(ipArr) // BenchmarkIp-4                302           3414727 ns/op          801432 B/op      43729 allocs/op
+		radixTree.RadixTest(ipArr) // BenchmarkIp-4               2467            533437 ns/op          639043 B/op         13 allocs/op
 	}
 }
 
