f337a5dfaf9317f11062b8da48f9a14853c26a47
pingerr <lizhaoping666@gmail.com>
2023-08-03 23:02:06 +0800

    feat:cc(bucket,muli)+ip(nradix)+waf(best+libsqli+pcre)

--- waf_deny/pingerPlugins/pingerOperators.go
+++ waf_deny/pingerPlugins/pingerOperators.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"github.com/corazawaf/coraza/v3/experimental/plugins"
 	"github.com/corazawaf/coraza/v3/experimental/plugins/plugintypes"
-	"go.elara.ws/pcre"
 )
 
 type rx struct {
-	re *pcre.Regexp
+	re Regexp
 }
 
 var _ plugintypes.Operator = (*rx)(nil)
@@ -19,14 +18,14 @@ func newRX(options plugintypes.OperatorOptions) (plugintypes.Operator, error) {
 
 	//var re *pcre.Regexp
 	//re := pcre.MustCompile(data, 0)
-	re := pcre.MustCompile(data)
+	re := MustCompile(data, 0)
 
 	return &rx{re: re}, nil
 }
 
 func (o *rx) Evaluate(tx plugintypes.TransactionState, value string) bool {
 
-	return o.re.MatchString(value)
+	return o.re.MatchString(value, 0)
 
 }
 
