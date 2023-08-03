1e163daf5233c61c10dcd04e453b6f94767253ab
pingerr <lizhaoping666@gmail.com>
2023-08-03 20:55:15 +0800

    feat:cc(bucket,muli)+ip(nradix)+waf(best+libsqli+pcre)

--- waf_deny/go.mod
+++ waf_deny/go.mod
@@ -3,10 +3,10 @@ module waf_deny
 go 1.19
 
 require (
-	github.com/GRbit/go-pcre v1.0.0
 	github.com/alibaba/higress/plugins/wasm-go v0.0.0-20230708135159-28892cf3ae0f
 	github.com/corazawaf/coraza-wasilibs v0.0.0-20230620081031-05a5097dbea3
 	github.com/corazawaf/coraza/v3 v3.0.2
+	github.com/rubrikinc/go-pcre v0.0.0-20190614221902-2375a3cf1302
 	github.com/tetratelabs/proxy-wasm-go-sdk v0.22.0
 	github.com/tidwall/gjson v1.14.4
 	github.com/wasilibs/nottinygc v0.2.0
--- waf_deny/pingerPlugins/pingerOperators.go
+++ waf_deny/pingerPlugins/pingerOperators.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"github.com/corazawaf/coraza/v3/experimental/plugins"
 	"github.com/corazawaf/coraza/v3/experimental/plugins/plugintypes"
+	"github.com/rubrikinc/go-pcre"
 )
 
 type rx struct {
-	re Regexp
+	re pcre.Regexp
 }
 
 var _ plugintypes.Operator = (*rx)(nil)
@@ -17,7 +18,7 @@ func newRX(options plugintypes.OperatorOptions) (plugintypes.Operator, error) {
 	data := fmt.Sprintf("(?sm)%s", options.Arguments)
 
 	//var re *pcre.Regexp
-	re := MustCompile(data, 0)
+	re := pcre.MustCompile(data, 0)
 	//re := pcre.MustCompile(data, 0)
 
 	return &rx{re: re}, nil
@@ -25,7 +26,7 @@ func newRX(options plugintypes.OperatorOptions) (plugintypes.Operator, error) {
 
 func (o *rx) Evaluate(tx plugintypes.TransactionState, value string) bool {
 
-	return o.re.MatchString(value, 0)
+	return o.re.MatcherString(value, 0).Matches()
 
 }
 
