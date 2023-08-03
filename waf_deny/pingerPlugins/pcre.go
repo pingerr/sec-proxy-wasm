7b42c26393356c41ebba0a258452befcf0025f3d
pingerr <lizhaoping666@gmail.com>
2023-08-03 20:48:00 +0800

    feat:cc(bucket,muli)+ip(nradix)+waf(best+libsqli+pcre)

--- waf_deny/pingerPlugins/pcre.go
+++ waf_deny/pingerPlugins/pcre.go
@@ -1,4 +1,4 @@
-package utils
+package pingerPlugins
 
 /*
 #cgo pkg-config: pcre
--- waf_deny/pingerPlugins/pingerOperators.go
+++ waf_deny/pingerPlugins/pingerOperators.go
@@ -2,15 +2,12 @@ package pingerPlugins
 
 import (
 	"fmt"
-	"github.com/GRbit/go-pcre"
 	"github.com/corazawaf/coraza/v3/experimental/plugins"
 	"github.com/corazawaf/coraza/v3/experimental/plugins/plugintypes"
-	//"github.com/GRbit/go-pcre"
-	_ "waf_deny/pingerPlugins/utils"
 )
 
 type rx struct {
-	re pcre.Regexp
+	re Regexp
 }
 
 var _ plugintypes.Operator = (*rx)(nil)
@@ -20,7 +17,7 @@ func newRX(options plugintypes.OperatorOptions) (plugintypes.Operator, error) {
 	data := fmt.Sprintf("(?sm)%s", options.Arguments)
 
 	//var re *pcre.Regexp
-	re := pcre.MustCompile(data, 0)
+	re := MustCompile(data, 0)
 	//re := pcre.MustCompile(data, 0)
 
 	return &rx{re: re}, nil
