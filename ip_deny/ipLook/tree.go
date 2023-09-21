b67dd9cf553f57ca17187d83a6e2c780f0e9d735
work <429304117@qq.com>
2023-09-21 17:13:10 +0800

    feat:增加注释

--- ip_deny/ipLook/tree.go
+++ ip_deny/ipLook/tree.go
@@ -5,31 +5,29 @@ import (
 	"net"
 )
 
-// bitslen 常数表示每个节点存储bit子串的长度，必须是2的指数次幂
-// 当 bitslen 为 4 时，最大跳数将为32 / bitslen，一个节点占用1<< bitslen *（SID和*treenode的大小）。
-// 因此，当前常数（4）最大跳数为8
-// IP subnet 的前缀长度只能是 bitslen的倍数，当前bitslen(4)的 subnet的前缀长度只能是4、8、12、16...
-const bitslen = 4
+// bitsLen 常数表示每个节点存储bit子串的长度，必须是2的指数次幂,可根据subnet前缀长度重新指定
+// 当 bitsLen 为 4 时，最大跳数(树的深度)将为32 / bitsLen.
+const bitsLen = 4
 
 // SID SID表示对应节点的状态，每一位表示1种状态。
-// uint8 表示可以存储8个状态。0表示没有服务。
+// uint8 表示可以存储8个状态。
 // ip 黑名单场景，只用了1和0两个状态
 type SID uint8
 
 type Tree struct {
-	root *treenode
+	root *treeNode
 }
 
-// treenode bit树节点
-// 如果每个节点保存4个bit（bitslen=4）的信息，即"0 or 1, 0 or 1, 0 or 1, 0 or 1",则有1<<4= 16种排列组合，相当于16叉树
-type treenode struct {
-	srvs [1 << bitslen]SID
-	ptrs [1 << bitslen]*treenode
+// treeNode bit树节点
+// 如果每个节点保存4个bit（bitsLen=4）的信息，即"0 or 1, 0 or 1, 0 or 1, 0 or 1",则有1<<4= 16种排列组合
+type treeNode struct {
+	srvs [1 << bitsLen]SID
+	ptrs [1 << bitsLen]*treeNode
 }
 
 // New 建立新的IP subnet tree，仅实现IPv4。
 func New() *Tree {
-	tree := &Tree{&treenode{}}
+	tree := &Tree{&treeNode{}}
 	return tree
 }
 
@@ -40,10 +38,10 @@ func (tree *Tree) Add(service SID, ipnet net.IPNet) {
 	node := tree.root
 
 	prefixLen, _ := ipnet.Mask.Size() //比如24
-	curLen := bitslen                 //4
-	for i := 0; i < 32/bitslen; i++ { //i<8
+	curLen := bitsLen                 //4
+	for i := 0; i < 32/bitsLen; i++ { //i范围[0,8)
 		if curLen >= prefixLen {
-
+			//当前节点为叶子结点，填充状态值
 			start := getSubstring(ipnet.IP, uint8(i))
 			end := start + (1 << uint(curLen-prefixLen)) - 1
 
@@ -57,10 +55,10 @@ func (tree *Tree) Add(service SID, ipnet net.IPNet) {
 		if node.ptrs[ind] != nil {
 			node = node.ptrs[ind]
 		} else {
-			node.ptrs[ind] = &treenode{}
+			node.ptrs[ind] = &treeNode{}
 			node = node.ptrs[ind]
 		}
-		curLen += bitslen
+		curLen += bitsLen
 	}
 }
 
@@ -69,9 +67,9 @@ func (tree *Tree) Get(ipv4 []byte) SID {
 	var ans SID
 	cur := tree.root
 
-	for i := 0; i < 32/bitslen; i++ {
+	for i := 0; i < 32/bitsLen; i++ {
 		ind := getSubstring(ipv4, uint8(i))
-		ans = ans | cur.srvs[ind]
+		ans = ans | cur.srvs[ind] //非叶子节点的ans都为0
 		if cur = cur.ptrs[ind]; cur == nil {
 			break
 		}
@@ -80,12 +78,12 @@ func (tree *Tree) Get(ipv4 []byte) SID {
 	return ans
 }
 
-// getSubstring 获取在区间 [index * bitslen, index * bitslen + bitslen) 内的bit子串的值
+// getSubstring 获取在区间 [index * bitsLen, index * bitsLen + bitsLen) 内的bit子串的10进制值
 // 例如. 11101111111111111111111111111111, 区间[0, 4)，子串为1110，结果为 14
 func getSubstring(ipv4 []byte, index uint8) uint32 {
 	var ans = binary.BigEndian.Uint32(ipv4)
-	ans = ans << (bitslen * index)
-	ans = ans >> (32 - bitslen)
+	ans = ans << (bitsLen * index)
+	ans = ans >> (32 - bitsLen)
 	return ans
 }
 
